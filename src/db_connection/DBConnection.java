292a191c4d4e3609bfb8db7f1bcd0bd8a03f5199
Noah do Rego <ndore051@uottawa.ca>
2023-03-13 16:06:52 -0400

    Set up db connection and create table command

--- src/db_connection/DBConnection.java
+++ src/db_connection/DBConnection.java
@@ -1,9 +1,12 @@
 package src.db_connection;
 
+import java.sql.Connection;
+
 public class DBConnection {
 
     public static void main(String[] args) {
         DBFunctions db = new DBFunctions();
-        db.connectToDB("ehotels_db", "postgres", "gumbly456!");
+        Connection connection = db.connectToDB("postgres", "ehotels_db", "postgres", "gumbly456!");
+        db.createTable(connection, "test");
     }
 }
--- src/db_connection/DBFunctions.java
+++ src/db_connection/DBFunctions.java
@@ -2,15 +2,17 @@ package src.db_connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class DBFunctions {
 
-    public Connection connectToDB(String dbname, String username, String password) {
+    public Connection connectToDB(String dbname, String schema, String username, String password) {
         Connection connect = null;
 
         try {
             Class.forName("org.postgresql.Driver");
-            connect = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbname, username, password);
+            connect = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/" + dbname + "?currentSchema=" + schema, username, password);
 
             if (connect != null) {
                 System.out.println("Connection successful.");
@@ -24,5 +26,17 @@ public class DBFunctions {
         }
 
         return connect;
+    }
+
+    public void createTable(Connection connect, String tableName) {
+        Statement statement;
+        try {
+            String query = "CREATE TABLE " + tableName + "(test TEXT, number INT,PRIMARY KEY(test));";
+            statement = connect.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table created");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
