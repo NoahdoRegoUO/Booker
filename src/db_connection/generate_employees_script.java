68c8a393090736d932b0986bd32c26f7d4530160
Noah do Rego <ndore051@uottawa.ca>
2023-03-13 16:11:20 -0400

    Refactored file structure

--- src/db_connection/generate_employees_script.java
+++ src/db_connection/generate_employees_script.java
@@ -1,24 +1,26 @@
+package src.db_connection;
 // NOTE: THIS CODE WILL NOT WORK ON IT'S OWN, IT MUST BE ALONGSIDE A MAVEN PROJECT THAT IMPLEMENTS THE LIBRARY.
+
 // FAKER LIBRARY -> https://github.com/DiUS/java-faker
 
 import com.github.javafaker.Faker;
 import java.lang.Math;
 
 public class generate_employees_script {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String query = "";
 
-        for(int i = 0; i < 220; i++){
+        for (int i = 0; i < 220; i++) {
             int employeeid = i + 1;
 
-//            String position = "'Guest Services'";
-//
-//            // get a manager for the hotel
-//            if((i % 5) + 1 == 1){
-//                position = "'Manager'";
-//            }
+            // String position = "'Guest Services'";
+            //
+            // // get a manager for the hotel
+            // if((i % 5) + 1 == 1){
+            // position = "'Manager'";
+            // }
 
-            int sin = 100000000 + (int)(Math.random() * 900000000);
+            int sin = 100000000 + (int) (Math.random() * 900000000);
 
             Faker faker = new Faker();
 
@@ -30,18 +32,20 @@ public class generate_employees_script {
 
             employeeaddress = "'" + faker.address().streetAddress();
 
-            if(i < 85){
+            if (i < 85) {
                 employeeaddress = employeeaddress + ", Ottawa, Ontario" + "'";
-            } else{
+            } else {
                 employeeaddress = employeeaddress + ", Toronto, Ontario" + "'";
             }
 
             // generate age between 16 and 70
-            int age = (int)(Math.random() * 54 + 16);
+            int age = (int) (Math.random() * 54 + 16);
 
-            int hotelid = i/5 + 1; 
+            int hotelid = i / 5 + 1;
 
-            query = "INSERT INTO employee(employeeid, sin, fullname, employeeaddress, age, hotelid) VALUES ("+employeeid+", "+sin+", "+fullname+", "+employeeaddress+", "+age+", "+hotelid+");";
+            query = "INSERT INTO employee(employeeid, sin, fullname, employeeaddress, age, hotelid) VALUES ("
+                    + employeeid + ", " + sin + ", " + fullname + ", " + employeeaddress + ", " + age + ", " + hotelid
+                    + ");";
 
             System.out.println(query);
         }
--- src/db_connection/generate_rooms_script.java
+++ src/db_connection/generate_rooms_script.java
@@ -1,46 +1,48 @@
+package src.db_connection;
+
 import java.text.DecimalFormat;
 
 public class generate_rooms_script {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String query = "";
 
-        for(int i = 0; i < 220; i++){
+        for (int i = 0; i < 220; i++) {
 
-            int roomnumber = i+1;
+            int roomnumber = i + 1;
 
             double price = 1.00;
 
-            int capacity = (i % 5)+1;
+            int capacity = (i % 5) + 1;
 
             boolean occupied = false;
 
             String amenities = "";
 
             boolean extendable = true;
-            
+
             String view = "";
 
             String issues = "ARRAY['None']";
 
-            if(capacity == 1){
+            if (capacity == 1) {
                 amenities = "ARRAY['None']";
-                extendable = false; 
+                extendable = false;
                 price = Math.random() * 50 + 50;
 
-            } else if(capacity == 2){
+            } else if (capacity == 2) {
                 amenities = "ARRAY['Air Conditioning']";
                 extendable = false;
                 price = Math.random() * 50 + 100;
 
-            } else if (capacity == 3){
+            } else if (capacity == 3) {
                 amenities = "ARRAY['Air Conditioning', 'Television']";
                 price = Math.random() * 100 + 100;
 
-            } else if (capacity == 4){
+            } else if (capacity == 4) {
                 amenities = "ARRAY['Air Conditioning', 'Television', 'Microwave']";
                 price = Math.random() * 50 + 200;
 
-            } else if (capacity == 5){
+            } else if (capacity == 5) {
                 amenities = "ARRAY['Air Conditioning', 'Television', 'Microwave', 'Fridge']";
                 price = Math.random() * 50 + 300;
             }
@@ -48,23 +50,24 @@ public class generate_rooms_script {
             DecimalFormat df = new DecimalFormat("#.##");
             price = Double.valueOf(df.format(price));
 
-            int determineView = (int)(Math.random() * 2 + 1);
+            int determineView = (int) (Math.random() * 2 + 1);
 
-            if(determineView == 2){
+            if (determineView == 2) {
                 view = "'Sea'";
-            } else if(determineView == 1){
+            } else if (determineView == 1) {
                 view = "'Mountain'";
             }
 
-            int determineIssues = (int)(Math.random() * 50 + 1);
-            if(determineIssues >= 45){
+            int determineIssues = (int) (Math.random() * 50 + 1);
+            if (determineIssues >= 45) {
                 issues = "ARRAY['Slight Damage']";
             }
 
-            int hotelid = i/5 + 1; 
+            int hotelid = i / 5 + 1;
 
-
-            query = "INSERT INTO room(roomnumber, price, occupied, amenities, extendable, view, issues, capacity, hotelid) VALUES ("+roomnumber+", "+price+", false, "+amenities+", "+extendable+", "+view+", "+issues+", "+capacity+", "+hotelid+");";
+            query = "INSERT INTO room(roomnumber, price, occupied, amenities, extendable, view, issues, capacity, hotelid) VALUES ("
+                    + roomnumber + ", " + price + ", false, " + amenities + ", " + extendable + ", " + view + ", "
+                    + issues + ", " + capacity + ", " + hotelid + ");";
 
             System.out.println(query);
         }
