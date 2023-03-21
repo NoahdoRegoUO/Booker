7430caaa9e86020f1e10332693d3b21d395bccee
alexclements74 <aclem105@uottawa.ca>
2023-03-21 15:43:42 -0400

    Adding Basic Get Commands

--- src/server/index.js
+++ src/server/index.js
@@ -33,12 +33,111 @@ app.post("/testtable", async (req, res) => {
     }
 });
 
+// Get Archive
+app.get("/get-archives", async (req, res) => {
+    try {
+        await pool.query(setSchema);
+        const archiveData = await pool.query("SELECT * FROM archive");
+        res.json(archiveData.rows); 
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+// Get Bookings
+app.get("/get-bookings", async (req, res) => {
+    try {
+        await pool.query(setSchema);
+        const allBookings = await pool.query("SELECT * FROM booking");
+        res.json(allBookings.rows); 
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+// Get Central Offices
+app.get("/get-central-offices", async (req, res) => {
+    try {
+        await pool.query(setSchema);
+        const allCentralOffices = await pool.query("SELECT * FROM centraloffice");
+        res.json(allCentralOffices.rows); 
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+// Get Customers
+app.get("/get-customers", async (req, res) => {
+    try {
+        await pool.query(setSchema);
+        const allCustomers = await pool.query("SELECT * FROM customer");
+        res.json(allCustomers.rows); 
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+// Get Employees
+app.get("/get-employees", async (req, res) => {
+    try {
+        await pool.query(setSchema);
+        const allEmployees = await pool.query("SELECT * FROM employee");
+        res.json(allEmployees.rows); 
+    } catch (err) {
+        console.error(err);
+    }
+})
+
 // Get Hotels
 app.get("/get-hotels", async (req, res) => {
     try {
         await pool.query(setSchema);
         const allHotels = await pool.query("SELECT * FROM Hotel");
-        res.json(allHotels.rows); // treated as an array because "set" command is treated as first object
+        res.json(allHotels.rows); 
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+// Get Hotel Chains
+app.get("/get-hotel-chains", async (req, res) => {
+    try {
+        await pool.query(setSchema);
+        const allHotelChains = await pool.query("SELECT * FROM hotelchain");
+        res.json(allHotelChains.rows); 
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+// Get Positions
+app.get("/get-positions", async (req, res) => {
+    try {
+        await pool.query(setSchema);
+        const allPositions = await pool.query("SELECT * FROM position");
+        res.json(allPositions.rows); 
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+// Get Rentings
+app.get("/get-rentings", async (req, res) => {
+    try {
+        await pool.query(setSchema);
+        const allRentings = await pool.query("SELECT * FROM renting");
+        res.json(allRentings.rows); 
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+// Get Rooms
+app.get("/get-rooms", async (req, res) => {
+    try {
+        await pool.query(setSchema);
+        const allRooms = await pool.query("SELECT * FROM room");
+        res.json(allRooms.rows); 
     } catch (err) {
         console.error(err);
     }
