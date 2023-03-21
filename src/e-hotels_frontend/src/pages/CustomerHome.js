3eaea3e0639c4f31d04faee6b5574914621a6c98
Noah do Rego <ndore051@uottawa.ca>
2023-03-21 15:43:57 -0400

    Added npm search and dropdown components

--- src/e-hotels_frontend/src/pages/CustomerHome.js
+++ src/e-hotels_frontend/src/pages/CustomerHome.js
@@ -3,12 +3,38 @@ import React, { useState } from "react";
 import './styles/CustomerHome.css';
 import './styles/globalStyles.css';
 
-import Button from '@mui/material/Button';
+import { Link } from "react-router-dom";
+
+import Select from 'react-select';
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+]
 
 function CustomerHome() {
     return (
         <div className="page-background">
-            <p className="title-text">Customer</p>
+            <p className="title-text">Search Hotels</p>
+            <div id="search-info">
+                <p className="subtitle-text">Hotel Information</p>
+                <Select
+                    className="basic-single"
+                    classNamePrefix="select"
+                    defaultValue={"1"}
+                    isDisabled={false}
+                    isLoading={false}
+                    isClearable={true}
+                    isRtl={false}
+                    isSearchable={false}
+                    name="stars"
+                    options={options}
+                />
+            </div>
+            <div id="search-info">
+                <p className="subtitle-text">Room Information</p>
+            </div>
         </div>
     );
 }
