a6bd001cd466bf1420dcee255c840eefd8f7a1ae
alexclements74 <aclem105@uottawa.ca>
2023-03-28 17:10:09 -0400

    Added SQL Views and Indexes

--- src/sql/ddl_set_foreign_keys.sql
+++ src/sql/ddl_set_foreign_keys.sql
@@ -35,3 +35,38 @@ ADD FOREIGN KEY (RoomNumber) REFERENCES Room(RoomNumber);
 
 ALTER TABLE Renting
 ADD FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID);
+
+-- Commands for Creating Indexes
+CREATE INDEX idx_customer_fullname
+ON customer (fullname);
+
+CREATE INDEX idx_employee_fullname
+ON employee (fullname);
+
+CREATE INDEX idx_booking_customer_confirm
+ON booking (startdate, enddate, hotelid, customerid);
+
+-- Commands for Creating Views
+CREATE VIEW available_rooms_per_area AS 
+SELECT 
+  split_part(hoteladdress, ',', 2) as city,
+  COUNT(*) as num_available_rooms
+FROM 
+  room
+  JOIN hotel ON room.hotelid = hotel.hotelid
+WHERE 
+  occupied = false
+GROUP BY 
+  city;
+
+CREATE VIEW room_capacity_by_hotel AS
+SELECT 
+  hotel.hotelid, 
+  hotel.hotelname, 
+  SUM(room.capacity) as total_capacity
+FROM 
+  room
+  JOIN hotel ON room.hotelid = hotel.hotelid
+GROUP BY 
+  hotel.hotelid, 
+  hotel.hotelname;
